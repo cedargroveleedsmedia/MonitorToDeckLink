738a4d29eb40691a758962ef89c6730f28ce4c40
cedargroveleedsmedia <cedargroveleedsmedia@github.com>
2026-02-26 16:00:36 +0000

    Update inspectdecklink.bat to dump interface GUIDs

--- inspectdecklink.bat
+++ inspectdecklink.bat
@@ -1,18 +1,6 @@
 @echo off
-echo Inspecting DeckLink interop DLL methods on IDeckLinkVideoFrame...
+echo Inspecting DeckLink interop GUIDs...
 echo.
-
-:: Use ildasm if available, otherwise powershell reflection
-set "ILDASM="
-for /f "delims=" %%i in ('dir /s /b "C:\Program Files (x86)\Microsoft SDKs\Windows\*\x86\ildasm.exe" 2^>nul') do set "ILDASM=%%i"
-for /f "delims=" %%i in ('dir /s /b "C:\Program Files\Microsoft SDKs\Windows\*\x86\ildasm.exe" 2^>nul') do set "ILDASM=%%i"
-
-if defined ILDASM (
-    echo Using ildasm: %ILDASM%
-    "%ILDASM%" /text /pubonly "lib\DeckLinkAPI.dll" | findstr /i "GetBytes\|GetRowBytes\|IDeckLinkMutableVideo\|IDeckLinkVideo"
-) else (
-    echo ildasm not found, using PowerShell reflection...
-    powershell -Command "& { $a = [System.Reflection.Assembly]::LoadFile('%cd%\lib\DeckLinkAPI.dll'); $t = $a.GetTypes() | Where-Object { $_.Name -like '*DeckLinkMutableVideo*' -or $_.Name -like '*DeckLinkVideo*' }; foreach ($type in $t) { Write-Host '--- ' $type.FullName; $type.GetMethods() | ForEach-Object { Write-Host '  ' $_.Name } } }"
-)
+powershell -Command "& { $a = [System.Reflection.Assembly]::LoadFile('%cd%\lib\DeckLinkAPI.dll'); $types = $a.GetTypes() | Where-Object { $_.IsInterface -or $_.IsClass }; foreach ($t in $types) { $attr = $t.GetCustomAttributes([System.Runtime.InteropServices.GuidAttribute], $false); if ($attr.Length -gt 0) { Write-Host $t.Name '=' $attr[0].Value } } }"
 echo.
 pause
