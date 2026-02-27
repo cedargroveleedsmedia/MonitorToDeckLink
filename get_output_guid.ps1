38c00f1df4044aaa1337d2b1060f071227965f4f
cedargroveleedsmedia <cedargroveleedsmedia@github.com>
2026-02-27 00:41:54 +0000

    Update get_output_guid.ps1 to also copy DLL to project

--- get_output_guid.ps1
+++ get_output_guid.ps1
@@ -6,3 +6,9 @@ foreach ($t in $asm.GetTypes()) {
         Write-Host "$($t.Name) GUID: {$guid}"
     }
 }
+
+# Also copy the interop DLL to project directory
+$src = "$env:TEMP\DeckLinkInterop.dll"
+$dst = "C:\Users\TechOps\MonitorToDeckLink\DeckLinkInterop.dll"
+Copy-Item $src $dst -Force
+Write-Host "`nCopied interop DLL to project: $dst"
